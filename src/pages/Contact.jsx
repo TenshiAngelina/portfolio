896d522a2a9568c95e5f1c41a8d59066e0f2b108
TenshiAngelina <angelina.hcortes@gmail.com>
2023-07-08 20:59:07 -0600

    Add Linkedin logo at contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,10 @@
 const Contact = () => {
   return (
     <div>
-      <h2>Reach out to me at angelina.hcortes@gmail.com</h2>
-      <h2>For collaboration find me at <a href="https://www.linkedin.com/in/angelinahuertacortes/">LinkedIn</a></h2>
+      <h2>Reach out to me at <a href="mailto:angelina.hcortes@gmail.com">angelina.hcortes@gmail.com</a></h2>
+      <h2>Want to collaborate with me?
+      <a href="https://linkedin.com/in/angelinahuertacortes" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="angelinahuertacortes" height="30" width="40"/></a></h2>
+         
     </div>
   )
 }
