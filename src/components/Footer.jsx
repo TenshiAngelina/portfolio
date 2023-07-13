b92f77de3edca00a077c675dd8f0ccc212a95620
TenshiAngelina <angelina.hcortes@gmail.com>
2023-07-12 21:26:33 -0600

    Add images to footer, made some format corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 
 function Footer() {
   return (
@@ -7,9 +8,15 @@ function Footer() {
       <Container>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-center">
+          
           <Navbar.Text>
           Angelina Huerta Cortés Copyright© 2023
           </Navbar.Text>
+
+          <Navbar.Text>
+          <Image src="https://vitejs.dev/logo.svg" width="25" height="25"/> + <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" width="25" height="25"/>
+          </Navbar.Text>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
